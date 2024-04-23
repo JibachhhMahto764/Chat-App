a1c49a088677faaeec01a35d2a1d8c6be38c1ec0
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-04-23 09:04:02 +0545

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
- const express = require('express');
-
+ import express from "express";
+ import dotenv from "dotenv";
+ import authRoutes from "./routes/auth.routes.js";
  const app = express();
+
+
+ dotenv.config();
+ const PORT = process.env.PORT || 5000;
+ 
+
  app.get("/",(req, res) =>{
     // root route http:localhost:5000/
     res.send("Hello jibachh!!!");
  });
- app.listen(5000, () => console.log('Server Running on port 5000'));
+ app.use("/api/auth",authRoutes)
+ app.listen(PORT, () => console.log('Server Running on port ${PORT}')); 
